5d518b7acb06805d71e5f9c18656281744ed52fd
mvdm <mvdm@dartmouth.edu>
2018-08-08 21:57:06 -0400

    TAP version

--- RunAllTests.m
+++ RunAllTests.m
@@ -1,11 +1,33 @@
 import matlab.unittest.TestSuite;
+import matlab.unittest.TestRunner;
+import matlab.unittest.plugins.TAPPlugin;
+import matlab.unittest.plugins.ToFile;
 
 try
     suite = testsuite;
-    results = run(suite);
+    % Create a typical runner with text output
+    runner = TestRunner.withTextOutput();
+    % Add the TAP plugin and direct its output to a file
+    tapFile = fullfile(getenv('WORKSPACE'), 'testResults.tap');
+    runner.addPlugin(TAPPlugin.producingOriginalFormat(ToFile(tapFile)));
+    % Run the tests
+    results = runner.run(suite);
     display(results);
 catch e
     disp(getReport(e,'extended'));
     exit(1);
 end
-exit(any([results.Failed]));
+exit;
+
+%% non-TAP version
+% import matlab.unittest.TestSuite;
+% 
+% try
+%     suite = testsuite;
+%     results = run(suite);
+%     display(results);
+% catch e
+%     disp(getReport(e,'extended'));
+%     exit(1);
+% end
+% exit(any([results.Failed]));
